4d2de532e05a1960c42ae3587e991d8daba3bbd2
ebubeaso <aso.ebube1@outlook.com>
2021-10-15 11:57:22 -0400

    using the socket library for http and https requests

--- monitors.py
+++ monitors.py
@@ -1,12 +1,9 @@
 #! /usr/bin/env python
-# from icmplib import ping
 import requests
 import socket
 import time
 # This is a function for parsing ping requests
 def parse_ping(server):
-    # result = ping(address=server, count=2, privileged=False)
-    # average = round((result.avg_rtt), 1)
     ping_sum = 0
     start_time=time.time()
     for i in range(2):
@@ -17,7 +14,13 @@ def parse_ping(server):
 # This is a function for parsing HTTP requests
 def parse_http(server, port):
     # get the http response
-    response = requests.get(f"http://{server}:{port}")
-    # convert the elapsed time into milliseconds and round to one decimal place
-    response_time = round(response.elapsed.total_seconds() * 1000, 1)
+    start_time=time.time()
+    if int(port) == 443:
+        socket.getaddrinfo(server, port=443)
+        https_sum = ( (time.time() - start_time) * 1000 )
+        average = round( https_sum, 1 )
+        return average
+    socket.getaddrinfo(server, port=port)
+    http_sum = ( (time.time() - start_time) * 1000 )
+    response_time = round(http_sum, 1)
     return response_time
